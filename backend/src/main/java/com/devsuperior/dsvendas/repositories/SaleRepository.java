4b7b609c7bcc6376d94970ac19349f76b2561ac0
Rafaelrrb <rafaeborba@gmail.com>
2021-11-04 16:53:20 -0300

    Group by search

--- backend/src/main/java/com/devsuperior/dsvendas/repositories/SaleRepository.java
+++ backend/src/main/java/com/devsuperior/dsvendas/repositories/SaleRepository.java
@@ -1,9 +1,27 @@
 package com.devsuperior.dsvendas.repositories;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
+import java.util.List;
+
+import com.devsuperior.dsvendas.dto.SaleSuccessDTO;
+import com.devsuperior.dsvendas.dto.SaleSumDTO;
 import com.devsuperior.dsvendas.entities.Sale;
 
 public interface SaleRepository extends JpaRepository<Sale, Long> {
 
+  public static final String STRING = "SELECT new com.devsuperior.dsvendas.dto.SaleSumDTO(obj.seller,SUM(obj.amount))"
+      + "FROM Sale AS obj GROUP BY obj.seller";
+
+  @Query(STRING)
+
+  List<SaleSumDTO> amountGroupedBySeller();
+
+  public static final String STR = "SELECT new com.devsuperior.dsvendas.dto.SaleSuccessDTO(obj.seller,SUM(obj.visited),SUM(obj.deals))"
+      + "FROM Sale AS obj GROUP BY obj.seller";
+
+  @Query(STR)
+
+  List<SaleSuccessDTO> successGroupedBySeller();
 }
